f850c612c7d3f24b85d278c314c9fc89e7e4414f
Alessandro <alex3988@hotmail.com>
2015-05-09 01:04:36 +0200

    first commit

---
+++ MvvmTemplate/Template/ViewModels/DefaultOtherTabViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Template
+{
+    [Export(typeof(ITabItem))]
+    [ImplementPropertyChanged]
+    public class DefaultOtherTabViewModel : Screen, ITabItem 
+    {
+        public string Indirizzo { get; set; }
+
+        public DefaultOtherTabViewModel()
+        {
+            DisplayName = "Other Tab";
+
+            Indirizzo = "This Second Tab! Arghh!";
+        }
+
+    }
+}
---
+++ MvvmTemplate/Template/ViewModels/DefaultTabViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Template
+{
+    [Export(typeof(ITabItem))]
+    [ImplementPropertyChanged]
+    public class DefaultTabViewModel : Screen, ITabItem 
+    {
+        public string Indirizzo { get; set; }
+
+        public DefaultTabViewModel()
+        {
+            DisplayName = "Default Tab";
+
+            Indirizzo = "This first Tab! Arghh!";
+        }
+
+    }
+}
